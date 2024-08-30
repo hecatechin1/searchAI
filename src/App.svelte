7dd3683ede22ffadaad8f34f03340fff9314ed4a
HecateChin <wallace0079@foxmail.com>
2024-08-30 10:25:06 +0800

    test

--- src/App.svelte
+++ src/App.svelte
@@ -2,31 +2,32 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+
+
+
+
+
+
+
+  
 </script>
 
 <main>
+<!-- 测试的可以删掉 -->
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    <p>testt</p>
+  
+ <textarea>
+  testttestttestttestt
+ </textarea>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+ <button>发送</button>
+</div>
+<!-- 测试的可以删掉 -->
 </main>
+
+
+
 
 <style>
   .logo {
